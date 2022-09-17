47d2023679cc71eefe73c0ad41d36b3cddb2be26
Anthony Obiako <obiakoanthonygozie@gmail.com>
2022-09-17 13:44:32 +0100

    MongoDb database is Connected and models are set

--- Server/schema/schema.js
+++ Server/schema/schema.js
@@ -1,6 +1,29 @@
 const { projects, clients } = require('../sampleData.js')
 
-const { GraphQLObjectType, GraphQLString, GraphQLID, GraphQLSchema } = require('graphql');
+// Mongoose Models
+const Project = require('../models/Project');
+const Client = require('../models/Clients');
+
+
+const { GraphQLObjectType, GraphQLString, GraphQLID, GraphQLSchema, GraphQLList } = require('graphql');
+
+// Project type
+const ProjectType = new GraphQLObjectType({
+    name: 'Project',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString },
+        status: { type: GraphQLString },
+        client: {
+            type: ClientType,
+            resolve(parent, args) {
+                return clients.findById(parent.clientId);
+            },
+        },
+    }),
+});
+
 
 // Client type
 const ClientType = new GraphQLObjectType({
@@ -13,17 +36,37 @@ const ClientType = new GraphQLObjectType({
     })
 });
 
+
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
+        projects: {
+            type: new GraphQLList(ProjectType),
+            resolve(parent, args) {
+                return Project.find();
+            }
+        },
+        project: {
+            type: ProjectType,
+            args: { id: {type: GraphQLID } },
+            resolve(parent, args) {
+                return Project.findById(args.id);
+            }
+        },
         clients: {
+            type: new GraphQLList(ClientType),
+            resolve(parent, args) {
+                return Client.find();
+            }
+        },
+        client: {
             type: ClientType,
             args: { id: {type: GraphQLID } },
             resolve(parent, args) {
-                return clients.find(client => client.id === args.id);
-            }
-        }
-    }
+                return Client.findById(args.id);
+            },
+        },
+    },
 });
 
 module.exports = new GraphQLSchema({
