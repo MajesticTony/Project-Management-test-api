e0e8760321133ed3aa6cca67f1bc6691b68888a6
Anthony Obiako <obiakoanthonygozie@gmail.com>
2022-09-17 04:33:39 +0100

    dependencies installed and schemas servers are being set up

---
+++ Server/sampleData.js
@@ -0,0 +1,74 @@
+// Projects
+const projects = [
+    {
+        id: '1',
+        clientId: '1',
+        name: 'eCommerce Website',
+        description: 'Am in a field of dandelions wishing on everyone that you will be mine ah ah ah ah mine ah aha. wishing on Dandelions',
+        status: 'In Progress'
+    },
+    {
+        id: '2',
+        clientId: '2',
+        name: 'Betting App',
+        description: 'Am in a field of dandelions wishing on everyone that you will be mine ah ah ah ah mine ah aha. wishing on Dandelions',
+        status: 'In Progress'
+    },
+    {
+        id: '3',
+        clientId: '3',
+        name: 'Dating App',
+        description: 'Am in a field of dandelions wishing on everyone that you will be mine ah ah ah ah mine ah aha. wishing on Dandelions',
+        status: 'In Progress'
+    },
+    {
+        id: '4',
+        clientId: '4',
+        name: 'Auction Website',
+        description: 'Am in a field of dandelions wishing on everyone that you will be mine ah ah ah ah mine ah aha. wishing on Dandelions',
+        status: 'In Progress'
+    },
+    {
+        id: '5',
+        clientId: '5',
+        name: 'Design Prototype',
+        description: 'Am in a field of dandelions wishing on everyone that you will be mine ah ah ah ah mine ah aha. wishing on Dandelions',
+        status: 'In Progress'
+    },
+];
+
+// Clients
+const clients = [
+    {
+        id: '1',
+        name: 'Uzumaki Naruto',
+        email: 'seventhhokage@gmail.com',
+        phone: '123-456-7890',
+    },
+    {
+        id: '2',
+        name: 'Uchiha Sasuke',
+        email: 'thelastuchiha@gmail.com',
+        phone: '534-989-2587',
+    },
+    {
+        id: '3',
+        name: 'Minato Namikaze',
+        email: 'yellowflash@gmail.com',
+        phone: '218-089-0010',
+    },
+    {
+        id: '4',
+        name: 'Hatake Kakashi',
+        email: 'copyninja@gmail.com',
+        phone: '712-967-0001',
+    },
+    {
+        id: '5',
+        name: 'Uchiha Madara',
+        email: 'theghostoftheuchiha@gmail.com',
+        phone: '999-999-9999',
+    },
+];
+
+module.exports = { projects, clients};
